2d5e825d40f99dc53e2b759b8d0da58fc7e0c29e
Christopher Peckover <christopher.peckover@gmail.com>
2017-08-08 02:41:37 -0400

    Initial Commit

--- src/main/java/com/gazorpazorp/discoveryService/DiscoveryService.java
+++ src/main/java/com/gazorpazorp/discoveryService/DiscoveryService.java
@@ -1,14 +1,15 @@
-package com.gazorpazorp;
+package com.gazorpazorp.discoveryService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//Random Comment because changes
 @EnableEurekaServer
 @SpringBootApplication
-public class LITDiscoveryApplication {
+public class DiscoveryService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LITDiscoveryApplication.class, args);
+		SpringApplication.run(DiscoveryService.class, args);
 	}
 }
